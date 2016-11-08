94de9fe8b530cba64ca3a59b6cbd9f5645488eb3
Bram <bramsnoeklith@hotmail.com>
2016-11-08 12:13:47 +0100

    Implemented Chatclient, Message and User

--- src/com/company/shared/User.java
+++ src/com/company/shared/User.java
@@ -1,7 +1,27 @@
 package com.company.shared;
 
+import java.io.Serializable;
+
 /**
  * Coded by Ferry on 8-11-2016.
  */
-public class User {
+public class User implements Serializable {
+    private int id;
+    private String name;
+
+    public User(int id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
 }
